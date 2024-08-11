cc5d33483264bcaf91bb44e91fa8a7dcce2ef0c1
Mauro Rivera <mauroriverae@gmail.com>
2024-08-11 14:52:28 -0300

    Feat: 006 Sending registration message to rabbitmq

---
+++ register/src/Messenger/Message/UserRegisteredMessage.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Messenger\Message;
+
+class UserRegisteredMessage
+{
+    private string $name;
+    private string $email;
+
+    public function __construct(string $name, string $email)
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
